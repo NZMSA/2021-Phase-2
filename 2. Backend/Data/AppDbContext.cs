f654ae1406c36674b0ec0ddfa440d4ae44e7e175
ZHENK <chengzhenyang@gmail.com>
2021-06-16 11:50:30 +1200

    👷 Add queries

---
+++ 2. Backend/Data/AppDbContext.cs
@@ -0,0 +1,43 @@
+using Visual_Studio_Projects.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Visual_Studio_Projects.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Project>()
+                .HasOne(p => p.Student)
+                .WithMany(s => s.Projects)
+                .HasForeignKey(p => p.StudentId);
+
+            modelBuilder
+                .Entity<Project>()
+                .HasMany(p => p.Comments)
+                .WithOne(c => c.Project)
+                .HasForeignKey(p => p.ProjectId);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Student)
+                .WithMany(s => s.Comments)
+                .HasForeignKey(c => c.StudentId);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne(c => c.Project)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(c => c.ProjectId);
+        }
+    }
+}
---
+++ 2. Backend/GraphQL/Query.cs
@@ -0,0 +1,37 @@
+
+
+using System.Linq;
+using Visual_Studio_Projects.Data;
+using Visual_Studio_Projects.Models;
+using HotChocolate;
+using HotChocolate.Data;
+
+namespace Visual_Studio_Projects.GraphQL
+{
+    public class Query
+    {
+        [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Project> GetProject([ScopedService] AppDbContext context)
+        {
+            return context.Projects;
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Student> GetStudent([ScopedService] AppDbContext context)
+        {
+            return context.Students;
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        [UseFiltering]
+        [UseSorting]
+        public IQueryable<Comment> GetComment([ScopedService] AppDbContext context)
+        {
+            return context.Comments;
+        }
+    }
+}
