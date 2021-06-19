a864ec37641bfeedab4a20b1235ec09c423f36a8
Rodger Gu <rodger.gu@vulcan.co>
2021-06-19 12:36:19 +1200

    Moved stuff around

--- 1. Frontend/storybook-app/src/stories/Header/Header.stories.tsx
+++ 1. Frontend/storybook-app/src/stories/Header/Header.stories.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
-
 import { Header, HeaderProps } from './Header';
 
 export default {
--- 1. Frontend/storybook-app/src/stories/Header/Header.tsx
+++ 1. Frontend/storybook-app/src/stories/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Button } from '../Button/Button';
 
-import { Button } from './Button';
 import './header.css';
 
 export interface HeaderProps {
