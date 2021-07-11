968a0a1ce425a2794c38b5aa2f14537795ef4423
Rodger Gu <rgu663@aucklanduni.ac.nz>
2021-07-12 10:01:52 +1200

    Added in card component contents

--- 1. Frontend/storybook-app/src/stories/Cards/GithubCard.tsx
+++ 1. Frontend/storybook-app/src/stories/Cards/GithubCard.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
-import { Card } from '@material-ui/core';
+import { Card, CardActionArea, CardContent, CardHeader, Avatar } from '@material-ui/core';
 
-const GithubCard = () : JSX.Element => {
+export interface GithubCardProps {
+    avatar: typeof Avatar;
+    cardTitle: string;
+    subHeader: string;
+    cardContent: JSX.Element;
+    url: string;
+};
+
+const GithubCard = ({avatar, cardTitle, subHeader, cardContent, url} : GithubCardProps) : JSX.Element => {
+
     return <Card>
-        
+        <CardActionArea onClick={() => window.location.href = url}>
+            <CardHeader avatar={avatar} title={cardTitle} subHeader={subHeader} />
+            <CardContent>
+                {cardContent}
+            </CardContent>
+        </CardActionArea>
     </Card>
-}
+};
+
+export default GithubCard;
