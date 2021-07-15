7b1c81e5de25a1ee9d2a723b4e641fdd4441be37
Rodger Gu <rgu663@aucklanduni.ac.nz>
2021-07-16 09:36:44 +1200

    Added in actual page

---
+++ 1. Frontend/storybook-app/src/FeedPage.tsx
@@ -0,0 +1,31 @@
+import { makeStyles, createStyles } from '@material-ui/core';
+import React from 'react';
+import { CardList, SectionHeader } from './stories';
+
+const FeedPageStyles = makeStyles(createStyles({
+    header: {
+        position: 'sticky'
+    }
+}));
+
+export interface FeedPageProps {
+    pageTitle: string;
+};
+
+const FeedPage = ({pageTitle} : FeedPageProps) : JSX.Element => {
+    const [cards, setCards] = React.useState<JSX.Element[]>([]);
+    const styles = FeedPageStyles();
+
+    React.useEffect(() => {
+        //TODO: Hook apollo client call into here.
+    }, []);
+
+    return <div>
+            <div className={styles.header}>
+                <SectionHeader sectionTitle={pageTitle} doClick={() => {}}/>
+            </div>
+            <CardList cards={cards} cols={2}/>
+        </div>
+}
+
+export default FeedPage;
