c73cf6440f9dea3bd8d02b5dc4473476cd32fd02
road2paradise <ken_nggg@outlook.com>
2021-07-18 15:37:39 +1200

    Refactored constants to a resources.tsx file

--- 1. Frontend/storybook-app/src/stories/Footer/Footer.tsx
+++ 1. Frontend/storybook-app/src/stories/Footer/Footer.tsx
@@ -1,55 +1,11 @@
 import React from 'react';
-import microsoftLogo from "../assets/logos/microsoft_logo.svg"
-import facebookLogo from "../assets/logos/facebook_logo.svg"
-import discordLogo from "../assets/logos/discord_logo.svg"
-import facebookgroupLogo from "../assets/logos/facebookgroup_logo.svg"
-import youtubeLogo from "../assets/logos/youtube_logo.svg"
-import githubLogo from "../assets/logos/github_logo.svg"
 import { FooterSocialIcon, SocialIconProps } from '../SocialIcon/SocialIcon';
-
-import { Box, Container, Grid, Hidden, Link, Typography } from '@material-ui/core';
-
+import { Box, Grid, Hidden, Link } from '@material-ui/core';
+import { SOCIAL_MEDIA, MICROSOFT_LOGO, PRIVACY_POLICY, TERMS_OF_USE } from "../assets/resources";
 
 import './footer.css';
 
-const SOCIAL_MEDIA = [
-    {
-        name: "Facebook",
-        url: "https://www.facebook.com/studentaccelerator/",
-        logo: facebookLogo
-    },
-    {
-        name: "Discord",
-        url: "https://discord.gg/c4Y5SAZ",
-        logo: discordLogo
-    },
-    {
-        name: "Facebook Group",
-        url: "https://www.facebook.com/groups/msaccelerator/",
-        logo: facebookgroupLogo
-    },
-    {
-        name: "YouTube",
-        url: "https://www.youtube.com/channel/UCCegNuS_AZjK-P3ZMN3JXNw",
-        logo: youtubeLogo
-    },
-    {
-        name: "GitHub",
-        url: "https://github.com/nzmsa",
-        logo: githubLogo
-    }
-]
-
-const MICROSOFT_LOGO = {
-    name: "Microsoft logo",
-    url: "https://www.microsoft.com/en-nz",
-    logo: microsoftLogo
-}
-const PRIVACY_POLICY = "https://go.microsoft.com/fwlink/?LinkId=521839"
-const TERMS_OF_USE = "https://go.microsoft.com/fwlink/?LinkID=206977"
-
-
-export default function Footer() {
+export const Footer = () => {
 
     return (
         <footer className="footer">
