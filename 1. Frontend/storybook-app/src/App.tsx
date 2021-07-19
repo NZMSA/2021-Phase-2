df28509282cf3f34c7b607960e2167cf5fcdcb41
road2paradise <ken_nggg@outlook.com>
2021-07-19 23:02:16 +1200

    Implemented router wrapper and basic routing paths, updated sidebar to include submit link

--- 1. Frontend/storybook-app/src/App.tsx
+++ 1. Frontend/storybook-app/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
 import './App.css';
 import { Header } from './stories/Header/Header';
 
 function App() {
-  const token = (window.location.href.match(/\?code=(.*)/)) && (window.location.href.match(/\?code=(.*)/) ?? [1]);
+  // TODO token parser from window - for oauth
+  // const token = (window.location.href.match(/\?code=(.*)/)) && (window.location.href.match(/\?code=(.*)/) ?? [1]);
   return (
     <div className="App">
       <Header />
-      {`${token}`}
+      <Switch>
+        <Route path="/home">
+          {/* Render cards here */}
+        </Route>
+        <Route path="/submit">
+          {/* Render submit page here*/}
+        </Route>
+      </Switch>
     </div>
   );
 }
--- 1. Frontend/storybook-app/src/stories/Sidebar/Sidebar.tsx
+++ 1. Frontend/storybook-app/src/stories/Sidebar/Sidebar.tsx
@@ -1,45 +1,45 @@
 import React from 'react';
 import { Divider, Link, List, ListItem, ListItemIcon, ListItemText, makeStyles } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
-import SettingsIcon from '@material-ui/icons/Settings';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const useStyles = makeStyles({
-    list: {
-      width: 250,
-    },
-    listText: {
-        color: "black"
-    },
-    fullList: {
-      width: 'auto',
-    },
-  });
+  list: {
+    width: 250,
+  },
+  listText: {
+    color: "black"
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
 
 // Custom Link composition component
 
 
 export const SideBar = () => {
-    const classes = useStyles();
-    return (
-        <div className={classes.list}>
-        <List>
-            <ListItem button href="/home" component={Link}>
-              <ListItemIcon><HomeIcon/></ListItemIcon>
-              <ListItemText className={classes.listText} primary="Home" />
-            </ListItem>
-        </List>
-        <Divider />
-        <List>
-            <ListItem button href="/settings" component={Link}>
-                <ListItemIcon><SettingsIcon/></ListItemIcon>
-                <ListItemText className={classes.listText} primary="Settings" />
-            </ListItem>
-            <ListItem button href="/logout" component={Link}>
-                <ListItemIcon><ExitToAppIcon/></ListItemIcon>
-                <ListItemText className={classes.listText} primary="Logout" />
-            </ListItem>
-        </List>
-      </div>
-    )
+  const classes = useStyles();
+  return (
+    <div className={classes.list}>
+      <List>
+        <ListItem button href="/home" component={Link}>
+          <ListItemIcon><HomeIcon /></ListItemIcon>
+          <ListItemText className={classes.listText} primary="Home" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem button href="/submit" component={Link}>
+          <ListItemIcon><ArrowUpwardIcon /></ListItemIcon>
+          <ListItemText className={classes.listText} primary="Settings" />
+        </ListItem>
+        <ListItem button href="/logout" component={Link}>
+          <ListItemIcon><ExitToAppIcon /></ListItemIcon>
+          <ListItemText className={classes.listText} primary="Logout" />
+        </ListItem>
+      </List>
+    </div>
+  )
 }
