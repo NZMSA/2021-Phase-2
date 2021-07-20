31a013d7782d1fa137c4665f9024c140ac5cd5ec
road2paradise <ken_nggg@outlook.com>
2021-07-20 21:17:14 +1200

    fixed footer to bottom, cleaned up header and removed search feature

--- 1. Frontend/storybook-app/src/stories/Header/Header.tsx
+++ 1. Frontend/storybook-app/src/stories/Header/Header.tsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { AppBar, Avatar, Drawer, fade, Hidden, IconButton, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Avatar, Drawer, Hidden, IconButton, Toolbar, Typography } from '@material-ui/core';
 import { SideBar } from '../Sidebar/Sidebar';
 import MenuIcon from '@material-ui/icons/Menu';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
 import Button from '@material-ui/core/Button';
 import logo from "../assets/logos/msa_full_neg.svg"
 
-import './header.css';
 // Not sure if we are going to demonstrate both importing a css file and in component styling.
 // Maybe leave both options? We can explain both.
 
@@ -39,49 +36,10 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     title: {
       flexGrow: 1,
-      display: 'none',
-      [theme.breakpoints.up('sm')]: {
-        display: 'block'
-      }
+      marginLeft: '10px'
     },
     inputRoot: {
       color: 'inherit',
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
-        },
-      },
-    },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
     },
     userInformation: {
       display: 'flex',
@@ -92,7 +50,6 @@ const useStyles = makeStyles((theme: Theme) =>
       alignItems: 'center',
       display: 'flex',
     }
-
   }
   ),
 );
@@ -130,21 +87,6 @@ export const Header: React.FC<HeaderProps> = ({ user }) => {
           <Typography className={classes.title} variant="h5" noWrap>
             Microsoft Student Accelerator Project Submissions
           </Typography>
-          <Hidden smDown>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </div>
-          </Hidden>
           {user == null ?
             <Button color="inherit" href={`https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=user&redirect_uri=${REDIRECT_URI}`}>
               Login
