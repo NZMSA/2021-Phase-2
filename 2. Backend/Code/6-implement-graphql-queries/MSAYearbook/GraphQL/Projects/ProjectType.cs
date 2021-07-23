28caea4c3f6606355d301ec746fe4a7daa57ea29
ZHENK <chengzhenyang@gmail.com>
2021-07-24 07:38:10 +1200

    Add demo code startup and queries

---
+++ 2. Backend/Code/6-implement-graphql-queries/MSAYearbook/GraphQL/Projects/ProjectType.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using HotChocolate;
+using HotChocolate.Types;
+using MSAYearbook.Data;
+using MSAYearbook.Models;
+using MSAYearbook.GraphQL.Students;
+using MSAYearbook.GraphQL.Comments;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace MSAYearbook.GraphQL.Projects
+{
+    public class ProjectType : ObjectType<Project>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Project> descriptor)
+        {
+            descriptor.Field(p => p.Id).Type<NonNullType<IdType>>();
+            descriptor.Field(p => p.Name).Type<NonNullType<StringType>>();
+            descriptor.Field(p => p.Description).Type<NonNullType<StringType>>();
+            descriptor.Field(p => p.Link).Type<NonNullType<StringType>>();
+            descriptor.Field(p => p.Year).Type<NonNullType<EnumType<Year>>>();
+
+            descriptor
+                .Field(p => p.Student)
+                .ResolveWith<Resolvers>(r => r.GetStudent(default!, default!, default))
+                .UseDbContext<AppDbContext>()
+                .Type<NonNullType<StudentType>>();
+
+            descriptor
+                .Field(p => p.Comments)
+                .ResolveWith<Resolvers>(r => r.GetComments(default!, default!, default))
+                .UseDbContext<AppDbContext>()
+                .Type<NonNullType<ListType<NonNullType<CommentType>>>>();
+
+            descriptor.Field(p => p.Modified).Type<NonNullType<DateTimeType>>();
+            descriptor.Field(p => p.Created).Type<NonNullType<DateTimeType>>();
+
+        }
+
+        private class Resolvers
+        {
+            public async Task<Student> GetStudent(Project project, [ScopedService] AppDbContext context,
+                   CancellationToken cancellationToken)
+            {
+                return await context.Students.FindAsync(new object[] { project.StudentId }, cancellationToken);
+            }
+
+            public async Task<IEnumerable<Comment>> GetComments(Project project, [ScopedService] AppDbContext context,
+                CancellationToken cancellationToken)
+            {
+                return await context.Comments.Where(c => c.ProjectId == project.Id).ToArrayAsync(cancellationToken);
+            }
+        }
+    }
+}
---
+++ 2. Backend/Code/6-implement-graphql-queries/MSAYearbook/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MSAYearbook.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string GitHub { get; set; }
+
+        public string ImageURI { get; set; }
+
+        public ICollection<Project> Projects { get; set; } = new List<Project>();
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
