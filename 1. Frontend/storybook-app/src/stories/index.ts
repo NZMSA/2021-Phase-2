9e1e5ec459e30b34c95b118066ea0438a3912ec7
ZHENK <chengzhenyang@gmail.com>
2021-07-27 09:53:57 +1200

    rename components back to stories

--- 1. Frontend/storybook-app/src/stories/index.ts
+++
@@ -1,5 +0,0 @@
-import GithubCard, { GithubCardProps } from './Cards/GithubCard';
-import CardList, { CardListProps } from './CardList/CardList';
-import SectionHeader, { SectionHeaderProps } from './SectionHeader/SectionHeader';
-export {GithubCard, CardList, SectionHeader};
-export type {GithubCardProps, CardListProps, SectionHeaderProps};
