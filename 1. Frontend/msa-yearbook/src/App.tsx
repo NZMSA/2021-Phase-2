5055e9bf670f5e177d37069d3ba82528bc66eceb
ZHENK <chengzhenyang@gmail.com>
2021-07-26 15:25:38 +1200

    Restructure project

---
+++ 1. Frontend/msa-yearbook/src/App.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Switch } from "react-router";
+import "./App.css";
+
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import FeedPage from "./FeedPage";
+import { ApolloProvider } from "@apollo/client";
+import graphQLClient from "./GraphQLClient";
+
+function App() {
+  // TODO token parser from window - for oauth
+  // const token = (window.location.href.match(/\?code=(.*)/)) && (window.location.href.match(/\?code=(.*)/) ?? [1]);
+  return (
+    <ApolloProvider client={graphQLClient}>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/home">
+            <FeedPage pageTitle="MSA Projects 2021" />
+          </Route>
+          <Route path="/submit">
+            {/* TODO  
+          Render submit page here*/}
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
